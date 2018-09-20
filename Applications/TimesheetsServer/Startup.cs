ec8fcd07832ad0b248b3a9f0d806363ef1e8f552
simonOJ <simon.ouellet-joyal@desjardins.com>
2018-09-20 07:51:25 -0600

    Security

--- Applications/TimesheetsServer/Startup.cs
+++ Applications/TimesheetsServer/Startup.cs
@@ -10,6 +10,12 @@ using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
 using Steeltoe.Common.Discovery;
 using Timesheets;
 using Steeltoe.CircuitBreaker.Hystrix;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Steeltoe.Security.Authentication.CloudFoundry;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
 
 namespace TimesheetsServer
 {
@@ -26,11 +32,23 @@ namespace TimesheetsServer
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
-            services.AddMvc();
+             services.AddMvc(mvcOptions =>
+            {
+                if (!Configuration.GetValue("DISABLE_AUTH", false))
+                {
+                    // Set Authorized as default policy
+                    var policy = new AuthorizationPolicyBuilder(JwtBearerDefaults.AuthenticationScheme)
+                    .RequireAuthenticatedUser()
+                    .RequireClaim("scope", "uaa.resource")
+                   .Build();
+                    mvcOptions.Filters.Add(new AuthorizeFilter(policy));
+                }
+            });
 
             services.AddDbContext<TimeEntryContext>(options => options.UseMySql(Configuration));
             services.AddScoped<ITimeEntryDataGateway, TimeEntryDataGateway>();
             
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddSingleton<IProjectClient>(sp =>
             {
                 var handler = new DiscoveryHttpClientHandler(sp.GetService<IDiscoveryClient>());
@@ -39,12 +57,18 @@ namespace TimesheetsServer
                     BaseAddress = new Uri(Configuration.GetValue<string>("REGISTRATION_SERVER_ENDPOINT"))
                 };
 
+                var contextAccessor = sp.GetService<IHttpContextAccessor>();
                 var logger = sp.GetService<ILogger<ProjectClient>>();
-                return new ProjectClient(httpClient, logger);
+                return new ProjectClient(
+                     httpClient, logger,
+                     () => contextAccessor.HttpContext.GetTokenAsync("access_token")
+                 );
 
             });
             services.AddDiscoveryClient(Configuration);
             services.AddHystrixMetricsStream(Configuration);
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                 .AddCloudFoundryJwtBearer(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- IntegrationTest/FlowTest.cs
+++ IntegrationTest/FlowTest.cs
@@ -25,6 +25,7 @@ namespace IntegrationTest
                 .AppName("RegistrationServer")
                 .Port(8883)
                 .Database("tracker_registration_dotnet_test")
+                .SetEnvironmentVariable("DISABLE_AUTH", "true")
                 .SetEnvironmentVariable("EUREKA__CLIENT__SHOULDREGISTERWITHEUREKA", "false")
                 .Build();
 
@@ -32,6 +33,7 @@ namespace IntegrationTest
                 .AppName("AllocationsServer")
                 .Port(8881)
                 .Database("tracker_allocations_dotnet_test")
+                .SetEnvironmentVariable("DISABLE_AUTH", "true")
                 .SetEnvironmentVariable("REGISTRATION_SERVER_ENDPOINT", _registrationServer.Url())
                 .SetEnvironmentVariable("EUREKA__CLIENT__SHOULDFETCHREGISTRY", "false")
                 .Build();
@@ -40,6 +42,7 @@ namespace IntegrationTest
                 .AppName("BacklogServer")
                 .Port(8882)
                 .Database("tracker_backlog_dotnet_test")
+                .SetEnvironmentVariable("DISABLE_AUTH", "true")
                 .SetEnvironmentVariable("REGISTRATION_SERVER_ENDPOINT", _registrationServer.Url())
                 .SetEnvironmentVariable("EUREKA__CLIENT__SHOULDFETCHREGISTRY", "false")
                 .Build();
@@ -48,6 +51,7 @@ namespace IntegrationTest
                 .AppName("TimesheetsServer")
                 .Port(8884)
                 .Database("tracker_timesheets_dotnet_test")
+                .SetEnvironmentVariable("DISABLE_AUTH", "true")
                 .SetEnvironmentVariable("REGISTRATION_SERVER_ENDPOINT", _registrationServer.Url())
                 .SetEnvironmentVariable("EUREKA__CLIENT__SHOULDFETCHREGISTRY", "false")
                 .Build();
